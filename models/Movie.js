f049c0d15451360449a9612179e44b68d4bd466e
Gaurav Hegde <gaurav.hegde2021@vitstudent.ac.in>
2022-10-19 20:41:07 +0530

    feat: models added for User, Movies and List of Movies

---
+++ models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const MovieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String },
+    img: { type: String },
+    imgTitle: { type: String },
+    video: { type: String },
+    genre: { type: String },
+    isSeries: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Movie", MovieSchema);
